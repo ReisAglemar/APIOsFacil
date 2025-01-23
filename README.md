3f14865e024432fed5c58571e65b4356b817f2fd
Reis <aglemar.reis@gmail.com>
2025-01-23 11:11:47 -0300

    feat: estrura base do OsFacil

---
+++ README.md
@@ -0,0 +1,6 @@
+
+# OS Fácil
+#### Simplicidade e facilidade para gerar ordens de serviços.
+
+## Módulo para manter usuários
+
---
+++ src/test/java/com/APIosFacil/usuario/OsFacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.APIosFacil.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OsFacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
